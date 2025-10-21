354e38015eb984d80075f8aab8d9148fcfcd0e92
cwhaught <cwhaught@gmail.com>
2025-10-21 15:12:45 -0400

    New prompt for number guessing game with formatting.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use inquire::Confirm;
+use inquire::{Confirm, CustomType};
 
 fn main() {
-    if(play_game()){
-        println!("You won!");
+    if play_game() {
+        num_guessing_game();
     } else {
         println!("Pretend you're on the main screen of a game.");
     }
@@ -24,4 +24,22 @@ fn play_game() -> bool {
     }
 
     return false;
+}
+
+fn num_guessing_game() -> i8 {
+    let num_guessing_prompt = CustomType::<i8>::new ("Guess a number 1 - 100")
+        .with_formatter(&|i| format!("${:.2}", i))
+        .with_error_message("That is not an acceptable input, try again")
+        .prompt();
+
+    match num_guessing_prompt {
+        Ok(guess) => {
+            println!("Your number is {}", guess);
+            return guess;
+        },
+        Err(_) => {
+            println!("ohhh no");
+            return -1;
+        },
+    }
 }
