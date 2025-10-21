c7afaf2be31c5bb3b1af9f61e7be1586a0b4bde8
cwhaught <cwhaught@gmail.com>
2025-10-21 14:12:49 -0400

    Moved initial prompt to its own function.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 use inquire::Confirm;
 
 fn main() {
-    let mut play_game = false;
+    if(play_game()){
+        println!("You won!");
+    } else {
+        println!("Pretend you're on the main screen of a game.");
+    }
+}
 
-    let ans = Confirm::new("Do you want to play a guessing game?")
+fn play_game() -> bool {
+    let play_game_prompt = Confirm::new("Do you want to play a guessing game?")
         .with_default(false)
         .prompt();
 
-    match ans {
+    match play_game_prompt {
         Ok(true) => {
             println!("Great, that's what I am going to figure out next.");
 
-            play_game = true;
+            return true;
         },
         Ok(false) => println!("Well that's easy, I don't have a game yet anyway."),
         Err(_) => println!("ohhh noe"),
     }
 
-    if(play_game){
-        println!("You won!");
-    } else {
-        println!("Pretend you're on the main screen of a game.");
-    }
+    return false;
 }
