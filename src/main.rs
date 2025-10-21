d6ac1d8105f2d4e2ae9bd84830e99ed5306c489e
cwhaught <cwhaught@gmail.com>
2025-10-21 14:08:57 -0400

    Added inquire confirm for some basic command line prompting.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use inquire::Confirm;
+
 fn main() {
-    println!("Hello, world!");
+    let mut play_game = false;
+
+    let ans = Confirm::new("Do you want to play a guessing game?")
+        .with_default(false)
+        .prompt();
+
+    match ans {
+        Ok(true) => {
+            println!("Great, that's what I am going to figure out next.");
+
+            play_game = true;
+        },
+        Ok(false) => println!("Well that's easy, I don't have a game yet anyway."),
+        Err(_) => println!("ohhh noe"),
+    }
+
+    if(play_game){
+        println!("You won!");
+    } else {
+        println!("Pretend you're on the main screen of a game.");
+    }
 }
