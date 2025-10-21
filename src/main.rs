aa468b7f408fc3bca0bb04ab7b7d234dba5ea2cb
cwhaught <cwhaught@gmail.com>
2025-10-21 18:24:30 -0400

    Working guessing game logic.  - Randomly generates a number between 1-100, excluding 67.  - Accepts repeated user guesses until the correct number is guessed along with basic validations.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use inquire::{Confirm, CustomType};
 use inquire::validator::Validation;
+use rand::Rng;
 
 fn main() {
     if play_game() {
@@ -15,12 +16,8 @@ fn play_game() -> bool {
         .prompt();
 
     match play_game_prompt {
-        Ok(true) => {
-            println!("Great, that's what I am going to figure out next.");
-
-            return true;
-        },
-        Ok(false) => println!("Well that's easy, I don't have a game yet anyway."),
+        Ok(true) => return true,
+        Ok(false) => println!("Lame. I didn't want to play with you anyway."),
         Err(_) => println!("ohhh noe"),
     }
 
@@ -28,23 +25,36 @@ fn play_game() -> bool {
 }
 
 fn num_guessing_game() -> i8 {
+    let random_num:i8 = loop {
+        let n = rand::rng().random_range(1..101);
+        if n != 67 {
+            break n;
+        }
+    };
+
     let num_guessing_prompt = CustomType::<i8>::new ("I'm thinking of a number between 1 - 100, try to guess it.")
         .with_formatter(&|i| format!("${:.2}", i))
         .with_error_message("Enter a number between 1 and 100")
-        .with_validator(|input:&i8| {
+        .with_validator(move |input:&i8| {
             if *input == 67 {
                 Ok(Validation::Invalid("Absolutely not, you know why".into()))
-            }else if *input > 0 && *input <= 100 {
+            } else if (*input > 0 && *input < 101) && *input != random_num {
+                if *input > random_num {
+                    Ok(Validation::Invalid("You guessed too high".into()))
+                } else {
+                    Ok(Validation::Invalid("You guessed too low".into()))
+                }
+            } else if *input == random_num {
                 Ok(Validation::Valid)
             } else {
-                Ok(Validation::Invalid("You must enter a value between 1 - 100".into()))
+                Ok(Validation::Invalid("Enter a value between 1 - 100".into()))
             }
         })
         .prompt();
 
     match num_guessing_prompt {
         Ok(guess) => {
-            println!("Your number is {}", guess);
+            println!("YOU GOT IT! My number was {}", guess);
             guess
         },
         Err(_) => {
